b576b920ba06d37fc30675ae72f2f6b608a32811
Matthew Borders <mborders@outlook.com>
2018-11-07 14:29:34 -0600

    Use global bible instance for unit tests to increase speed

--- bible_test.go
+++ bible_test.go
@@ -5,32 +5,26 @@ import (
 	"testing"
 )
 
-func TestTestament_GetBookNames(t *testing.T) {
-	b := NewBible()
+var b = NewBible()
 
+func TestTestament_GetBookNames(t *testing.T) {
 	names := b.OldTestament.GetBookNames()
 	assert.Equal(t, 46, len(names))
 }
 
 func TestTestament_GetBook(t *testing.T) {
-	b := NewBible()
-
 	book, err := b.OldTestament.GetBook(1)
 	assert.Nil(t, err)
 	assert.Equal(t, "Genesis", book.Title)
 }
 
 func TestTestament_GetBook_New(t *testing.T) {
-	b := NewBible()
-
 	book, err := b.NewTestament.GetBook(1)
 	assert.Nil(t, err)
 	assert.Equal(t, "Matthew", book.Title)
 }
 
 func TestTestament_GetBook_Invalid(t *testing.T) {
-	b := NewBible()
-
 	_, err := b.OldTestament.GetBook(0)
 	assert.Equal(t, "invalid book number", err.Error())
 
@@ -39,16 +33,12 @@ func TestTestament_GetBook_Invalid(t *testing.T) {
 }
 
 func TestTestament_GetChapter(t *testing.T) {
-	b := NewBible()
-
 	chap, err := b.OldTestament.GetChapter(1, 1)
 	assert.Nil(t, err)
 	assert.Equal(t, uint8(1), chap.ChapterNumber)
 }
 
 func TestTestament_GetChapter_InvalidBook(t *testing.T) {
-	b := NewBible()
-
 	_, err := b.OldTestament.GetChapter(0, 0)
 	assert.Equal(t, "invalid book number", err.Error())
 
@@ -57,8 +47,6 @@ func TestTestament_GetChapter_InvalidBook(t *testing.T) {
 }
 
 func TestTestament_GetChapter_InvalidChapter(t *testing.T) {
-	b := NewBible()
-
 	_, err := b.OldTestament.GetChapter(1, 0)
 	assert.Equal(t, "invalid chapter number", err.Error())
 
@@ -67,24 +55,18 @@ func TestTestament_GetChapter_InvalidChapter(t *testing.T) {
 }
 
 func TestTestament_GetVerse(t *testing.T) {
-	b := NewBible()
-
 	vers, err := b.OldTestament.GetVerse(1, 1, 4)
 	assert.Nil(t, err)
 	assert.Equal(t, "And God saw the light that it was good; and he divided the light from the darkness.", vers.Text)
 }
 
 func TestTestament_GetVerse_New(t *testing.T) {
-	b := NewBible()
-
 	vers, err := b.NewTestament.GetVerse(4, 8, 32)
 	assert.Nil(t, err)
 	assert.Equal(t, "And you shall know the truth, and the truth shall make you free.", vers.Text)
 }
 
 func TestTestament_GetVerse_InvalidChapter(t *testing.T) {
-	b := NewBible()
-
 	_, err := b.OldTestament.GetVerse(1, 0, 0)
 	assert.Equal(t, "invalid chapter number", err.Error())
 
@@ -93,8 +75,6 @@ func TestTestament_GetVerse_InvalidChapter(t *testing.T) {
 }
 
 func TestTestament_GetVerse_InvalidVerse(t *testing.T) {
-	b := NewBible()
-
 	_, err := b.OldTestament.GetVerse(1, 1, 0)
 	assert.Equal(t, "invalid verse number", err.Error())
 
